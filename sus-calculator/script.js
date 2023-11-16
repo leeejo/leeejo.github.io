56e67ef07aa79e8fb8730155f4cc012b8743ebc3
Jo <lee.peini@gmail.com>
2023-11-16 05:42:01 +0000

    update the calculator

--- sus-calculator/script.js
+++ sus-calculator/script.js
@@ -1,14 +1,41 @@
-function calculateAverage() {
-    var scores = document.getElementById("responses").value.split(",");
+function calculateSUS() {
+    var responsesInput = document.getElementById("responses");
+    var responses = responsesInput.value.split(",").map(Number);
 
-    // Convert scores to numbers and calculate the average
-    var total = 0;
-    for (var i = 0; i < scores.length; i++) {
-        total += parseFloat(scores[i]);
+    // Check if there are exactly 10 responses
+    if (responses.length !== 10) {
+        alert("Please enter responses for all 10 statements.");
+        return;
     }
 
-    var average = total / scores.length;
+    // Check if all responses are between 1 and 5
+    if (responses.some(response => response < 1 || response > 5)) {
+        alert("Please enter responses between 1 and 5.");
+        return;
+    }
+
+    // Calculate SUS score
+    var susScore = calculateSUSScore(responses);
 
     // Display the result
-    document.getElementById("average").innerText = average.toFixed(2);
+    document.getElementById("susScore").innerText = susScore.toFixed(2);
+}
+
+function calculateSUSScore(responses) {
+    // Calculate combined responses
+    var combinedResponses = [];
+    for (var i = 0; i < responses.length; i += 2) {
+        combinedResponses.push(responses[i] - 1);
+        combinedResponses.push(5 - responses[i + 1]);
+    }
+
+    // Calculate total score
+    var totalScore = combinedResponses.reduce(function (acc, val) {
+        return acc + val;
+    }, 0);
+
+    // Multiply by 2.5 to get the SUS score
+    var susScore = totalScore * 2.5;
+
+    return susScore;
 }
